c1745f504293d94c417edfde1ae3309c6cf9e889
Max Kharandziuk <max.kharandziuk@gmail.com>
2021-02-26 20:54:10 +0100

    add pseudolist

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,50 +1,46 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Container, Box, Grid } from "@material-ui/core";
 
 export default function App() {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-          </ul>
-        </nav>
+      <Container>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+            </ul>
+          </nav>
 
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
+          <Box mt={8}>
+            <Switch>
+              <Route path="/about">
+                <About />
+              </Route>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </Box>
+        </div>
+      </Container>
     </Router>
   );
 }
 
 function Home() {
   return (
-    <Container>
-      <Grid container direction="column" spacing={3}>
-        <Grid item>
-          <Header />
-        </Grid>
-        <Grid item>
-          <ReposGrid />
-        </Grid>
-      </Grid>
-      <Box mt={8}>
-        <Copyright />
-      </Box>
-    </Container>
+    <ul>
+      <li>Book 1</li>
+      <li>Book 2</li>
+    </ul>
   );
 }
 
